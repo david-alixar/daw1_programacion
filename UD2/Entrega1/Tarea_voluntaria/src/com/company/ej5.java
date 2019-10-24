01a026cc3f28c8ed878cb3c0bf74078da7b4a544
David González <alum.dgonzalezc@iesalixar.org>
2019-10-24 19:11:49 +0200

    trabajo_clase

--- UD2/Entrega1/Tarea_voluntaria/src/com/company/ej5.java
+++ UD2/Entrega1/Tarea_voluntaria/src/com/company/ej5.java
@@ -6,20 +6,23 @@ public class ej5 {
 
     public static void main(String[] args) {
 
+
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Introduzca la altura de la pirámide");
+        System.out.println("Introduzca la altura de la pirámide:");
         int altura = teclado.nextInt();
         int i;
         int k;
-        System.out.println("Introduzca un caracter para construir la pirámide");
-        int n = teclado.nextInt();
+        System.out.println("Introduzca un caracter para construir la pirámide:");
 
-        for (i= 0; i< altura; i++) {
+        String cadena;
+        cadena = teclado.next();
+
+       for (i= 0; i<= altura; i++) {
             for (k= altura - i; k> 0; k--) {
                 System.out.print(" ");
             }
             for (k= 0; k< i; k++) {
-                System.out.print(" "+n);
+                System.out.print(" "+cadena);
             }
             System.out.println("");
         }
