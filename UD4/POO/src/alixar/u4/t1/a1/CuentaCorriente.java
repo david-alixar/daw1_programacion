d6fa2671543ed327f3b6546555bced27da53ba24
David González <alum.dgonzalezc@iesalixar.org>
2019-11-26 17:00:00 +0100

    poo

---
+++ UD4/POO/src/alixar/u4/t1/a1/CuentaCorriente.java
@@ -0,0 +1,22 @@
+package alixar.u4.t1.a1;
+
+public class CuentaCorriente {
+
+    // Atributos
+    double saldo;
+    int limite_descubrimiento;
+    String nombre;
+    String dni;
+
+    // Metodos
+    public boolean sacar_dinero(double peticion) {
+        boolean resultado = false;
+        return resultado;
+    }
+
+    public void ingresar_dinero(double ingreso) {
+
+    }
+
+    public void mostrar_informacion() {}
+}
---
+++ UD4/POO/src/alixar/u4/t1/a1/principal.java
@@ -0,0 +1,10 @@
+package alixar.u4.t1.a1;
+
+public class principal {
+    public static void main(String[] args) {
+        CuentaCorriente c = new CuentaCorriente();
+        //
+        c.nombre = "David";
+        c.saldo = 100;
+    }
+}
