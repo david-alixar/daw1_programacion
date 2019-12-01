b9a4fcd4970b2a33825beec3864dc3cafb0189f3
David <alum.dgonzalezc@iesalixar.org>
2019-12-01 18:51:53 +0100

    tareas_pendientes2

--- UD3/Entrega1/Ejercicio3/src/com/company/ej3.java
+++ UD3/Entrega1/Ejercicio3/src/com/company/ej3.java
@@ -15,19 +15,18 @@ public class ej3 {
         int n1 = 0;
         int n2 = 1;
         String sup_vocal;
-
-        System.out.println("Su nombre y apellidos sin vocales quedarían: ");
-
+        String cadena_final = "";
+        
         while (n2 < longitud1 +1) {
             sup_vocal = cadena1.substring(n1, n2);
             if (sup_vocal.equalsIgnoreCase("a") || (sup_vocal.equalsIgnoreCase("e")) || (sup_vocal.equalsIgnoreCase("i")) || (sup_vocal.equalsIgnoreCase("o")) || (sup_vocal.equalsIgnoreCase("u")) || (sup_vocal.equalsIgnoreCase("á")) || (sup_vocal.equalsIgnoreCase("é")) || (sup_vocal.equalsIgnoreCase("í")) || (sup_vocal.equalsIgnoreCase("ó")) || (sup_vocal.equalsIgnoreCase("ú")) || (sup_vocal.equalsIgnoreCase("ü")))  {
             }
             else {
-                System.out.print(sup_vocal);
+                cadena_final = cadena_final + sup_vocal;
             }
             n1 ++;
             n2 ++;
         }
-
+        System.out.print("Su nombre y apellidos sin vocales quedarían: " + cadena_final);
     }
 }
