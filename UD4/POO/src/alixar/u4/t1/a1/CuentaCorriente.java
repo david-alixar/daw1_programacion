632bb979838464411deef16477c00696eecac779
David González <alum.dgonzalezc@iesalixar.org>
2019-12-02 16:59:38 +0100

    ejercicios_tema2

--- UD4/POO/src/alixar/u4/t1/a1/CuentaCorriente.java
+++ UD4/POO/src/alixar/u4/t1/a1/CuentaCorriente.java
@@ -9,14 +9,32 @@ public class CuentaCorriente {
     String dni;
 
     // Metodos
+    public CuentaCorriente(String nombre, String DNI) {
+        saldo = 0;
+        this.nombre = nombre;
+        dni = DNI;
+        limite_descubrimiento = -50;
+    }
     public boolean sacar_dinero(double peticion) {
         boolean resultado = false;
+        if ((saldo - peticion) >= limite_descubrimiento) {
+            saldo -= peticion;
+            resultado = true;
+            System.out.println("Se han sacado " + peticion + " euros");
+        }
+        else {
+            System.out.println("No se ha podido sacar dinero. Saldo insuficiente");
+        }
         return resultado;
     }
 
     public void ingresar_dinero(double ingreso) {
-
+saldo += ingreso;
     }
 
-    public void mostrar_informacion() {}
+    public void mostrar_informacion() {
+        System.out.println("La cuenta corriente pertenece a: " + nombre + "con DNI: " + dni);
+        System.out.println("Tiene un saldo de: " + saldo + " euros");
+        System.out.println("Tiene un límite de descubierto de: " + limite_descubrimiento);
+    }
 }
