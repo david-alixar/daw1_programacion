99d8ecaccc646429703b8ddbf22636ce2ad4a748
David González <alum.dgonzalezc@iesalixar.org>
2020-02-05 17:29:22 +0100

    tareas_java

--- UD4/POO/src/alixar/u5/t3/a4/Conjunto.java
+++ UD4/POO/src/alixar/u5/t3/a4/Conjunto.java
@@ -7,51 +7,51 @@ public class Conjunto {
     //Atributos
     private Integer [] conjunto;
     private int contador = 0;
+    private int cantidad;
 
     //Constructores
-    public Conjunto(){
-        conjunto = new Integer[10];
+    public Conjunto() {
+        cantidad = 10;
+        conjunto = new Integer[cantidad];
     }
     public Conjunto(int cantidad){
+        this.cantidad = cantidad;
         conjunto = new Integer[cantidad];
     }
 
     //Métodos
     public int cant_elementos (){
-        return conjunto.length + 1;
+        return contador;
     }
+
     public void insertarNum (int numero){
-        boolean repetido = false;
-        if (contador == 0){
-            conjunto[0] = numero;
-            contador++;
+        if (contador >= conjunto.length){
+            conjunto = Arrays.copyOf(conjunto, conjunto.length + 1);
         }
-        else {
-            for (int i = 0; i < contador; i++) {
-                if (numero == conjunto[i]) {
-                    repetido = true;
-                    break;
-                }
-                if (repetido = false) {
-                    conjunto[contador] = numero;
-                    contador++;
-                }
+        for (int i = 0; i < contador; i++) {
+            if (numero == conjunto[i]) {
+                return;
             }
         }
+        conjunto[contador++] = numero;
     }
 
     public void eliminarNum (int numero){
-        for (int i = 0; i <= contador; i++){
+        boolean encontrado = false;
+        for (int i = 0; i < contador-1; i++){
             if (numero == conjunto[i]){
-                conjunto [i] = null;
-                break;
+                encontrado = true;
+            }
+            if (encontrado) {
+                conjunto[i] = conjunto[i + 1];
             }
         }
+        conjunto[--contador] = null;
     }
 
     public boolean pertenece (int numero){
         boolean pertenece = false;
-        for (int i = 0; i <= contador; i++){
+        for (int i = 0; i < contador; i++){
             if (numero == conjunto[i]){
                 pertenece = true;
                 break;
@@ -59,8 +59,27 @@ public class Conjunto {
         }
         return pertenece;
     }
+    public int getContador(){
+        return contador;
+    }
+    public void anadir_conjunto (Conjunto con){
+        for (int i = 0; i< con.getContador(); i++){
+            this.insertarNum(con.conjunto[i]);
+        }
+    }
 
-    public void setConjunto(){
+    public void eliminar_del_conjunto (Conjunto con){
+        for (int i = 0; i< con.getContador(); i++){
+            for (int j = 0; j < contador; j++) {
+                if (con.conjunto[i] == conjunto[j]) {
+                    this.eliminarNum(conjunto[j]);
+                }
+
+            }
+        }
+    }
+
+    public void mostrar_conjunto(){
         System.out.println(Arrays.toString(conjunto));
     }
 }
--- UD4/POO/src/alixar/u5/t3/a4/principal.java
+++ UD4/POO/src/alixar/u5/t3/a4/principal.java
@@ -7,7 +7,24 @@ public class principal {
         c1.insertarNum(14);
         c1.insertarNum(1);
         c1.insertarNum(4);
+        c1.insertarNum(19);
+        c1.insertarNum(16);
+        c1.insertarNum(24);
+        c1.insertarNum(94);
+        c1.insertarNum(79);
+        c1.insertarNum(56);
+        c1.insertarNum(74);
+        c1.insertarNum(64);
         System.out.println(c1.cant_elementos());
-        c1.setConjunto();
+        c1.mostrar_conjunto();
+        c1.eliminarNum(19);
+        System.out.println(c1.cant_elementos());
+        c1.mostrar_conjunto();
+        System.out.println(c1.pertenece(19));
+        c2.insertarNum(99);
+        c2.anadir_conjunto(c1);
+        c2.mostrar_conjunto();
+        c2.eliminar_del_conjunto(c1);
+        c2.mostrar_conjunto();
     }
 }
